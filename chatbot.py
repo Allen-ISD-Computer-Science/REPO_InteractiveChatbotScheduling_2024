5e2a59d7cc6baa11b07294e2a3197a9a2398ce00
Sai Paladugu <srinagasaisiv.paladugu@student.allenisd.org>
2024-01-26 12:57:02 -0600

    Added Chatbot Responses for School and Work Co-authored-by: ReyMPerez <r.marcellus.perez@gmail.com> *Checks for keywords in user request *Separated school and work response pathways *Created function for school responses *Worked on task #6

--- chatbot.py
+++ chatbot.py
@@ -1,4 +1,5 @@
 from chatterbot import ChatBot
+import re
 
 bot = ChatBot('Scheduler Chatbot') 
 
@@ -21,11 +22,29 @@ trainer.train([
 
 name=input("Enter Your Name: ")
 print("Welcome to the Scheduler Bot Service! Let me know how can I help you?")
+def chatbot_response(request):
+  requestResponse = ('Sorry, I have not learned how to respond to your request.')
+  if re.search(r'\b(student|homework|school)\b', request, re.IGNORECASE):
+    requestResponse = 1
+  elif re.search(r'\b(work|office|meeting|meetings)\b', request, re.IGNORECASE):
+    requestResponse = 2
+  return requestResponse
+
+def request_school_response():
+  print( 'Ok. I understand that you need help with organzation related to school.' )
+  schoolEnd = input("When does your school end? ")
+
+  
 while True:
-    request=input(name+':')
-    if request=='Bye' or request =='bye':
-        print('Bot: Bye')
-        break
-    else:
-        response=bot.get_response(request)
-        print('Scheduler Chatbot:',response)
+  request=input(name+':')
+  if request=='Bye' or request =='bye':
+    print('Bot: Bye')
+    break
+  response = chatbot_response(request)
+  if response == 1:
+    request_school_response()
+  
+ 
+    
+    
+
