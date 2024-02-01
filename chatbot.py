34ddd306d15e7bd40e0ec9ee1399402a04a8993d
Sai Paladugu <srinagasaisiv.paladugu@student.allenisd.org>
2024-02-01 12:47:41 -0600

    Switching to Python NLP *Deleted Python ChatterBot files *Installed necessary libraries *Added make.sh and build.sh *Worked on task #23

--- chatbot.py
+++ chatbot.py
@@ -1,50 +1,7 @@
-from chatterbot import ChatBot
-import re
-
-bot = ChatBot('Scheduler Chatbot') 
-
-bot = ChatBot(
-    'Scheduler Chatbot',
-    storage_adapter='chatterbot.storage.SQLStorageAdapter',
-    database_uri='sqlite:///database.sqlite3'
-)
-
-from chatterbot.trainers import ListTrainer
-
-trainer = ListTrainer(bot)
-
-trainer.train([
-'Hello! How are you today?',
-'What describes your visit today?',
-'How may I help you today?',
-'Are you a student?'
-])
-
-name=input("Enter Your Name: ")
-print("Welcome to the Scheduler Bot Service! Let me know how can I help you?")
-def chatbot_response(request):
-  requestResponse = ('Sorry, I have not learned how to respond to your request.')
-  if re.search(r'\b(student|homework|school)\b', request, re.IGNORECASE):
-    requestResponse = 1
-  elif re.search(r'\b(work|office|meeting|meetings)\b', request, re.IGNORECASE):
-    requestResponse = 2
-  return requestResponse
-
-def request_school_response():
-  print( 'Ok. I understand that you need help with organzation related to school.' )
-  schoolEnd = input("When does your school end? ")
-
-  
-while True:
-  request=input(name+':')
-  if request=='Bye' or request =='bye':
-    print('Bot: Bye')
-    break
-  response = chatbot_response(request)
-  if response == 1:
-    request_school_response()
-  
- 
-    
-    
-
+import numpy as np
+class ChatBot():
+    def __init__(self, name):
+        print("----- starting up", name, "-----")
+        self.name = name
+if __name__ == "__main__":
+    ai = ChatBot(name="Scheduler Chatbot")
---
+++ make.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Halt on errors or undefined environment variables
+set -eu
+
+# Create local python environment
+python3 -m venv .venv
+source .venv/bin/activate
+
+# Install required packages
+python3 -m pip install \
+   gTTS \
+   transformers \
+   tensorflow
+   
+    
