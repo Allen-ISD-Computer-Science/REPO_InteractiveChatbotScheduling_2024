b3d97399c169324a6f475897e60aa52e505fb063
Sai Paladugu <srinagasaisiv.paladugu@student.allenisd.org>
2024-02-11 22:19:33 -0600

    Functional chatbot that can talk to the user and add tasks *Redesigned the entire structure of the chatbot file *User can set start time to begin their tasks *User can add tasks and they can ask it to display them *The tasks are added in a way that ensures that the previous task is completed *Worked on tasks #8, #20, #24

--- chatbot.py
+++ chatbot.py
@@ -1,38 +1,99 @@
-import numpy as np
-import re
+from nltk.chat.util import Chat, reflections
+from flask import Flask, render_template, request
+import pandas as pd
+import random
+import datetime
+import os
 
-class ChatBot():
-    def __init__(self, name):
-        print("----- starting up", name, "-----")
-        self.name = name
-if __name__ == "__main__":
-    ai = ChatBot(name="Scheduler Chatbot")
+
+app = Flask(__name__)
+# As each valid task is submitted by the user, it gets added to the tasks array which will be used to display the table 
+tasks = []
+studentStartTime = None
+currentTask = None
+
+# Matches the user input with the one of the responses listed in the pairs
+class SchedulerChatbot(Chat):
+  def respond(self, str):
+    for (userQuery, schedulerChatbotResponse) in self.pairs:
+      match = userQuery.match(str)
+      if match:
+        response = random.choice(schedulerChatbotResponse)
+        if callable(response):
+          return response(*match.groups())
+        else:
+          return response
+             
+
+pairs = [
+     [
+        r"Hello|Hi|Howdy|Hey",
+        ["Hello, How are you today?", "Nice to meet you!"]
+    ],
+       [
+        r"Bye|Quit|See you tommorow|That's all the help I need",
+        ["It was my pleasure helping you today!, Have a great day!", "Bye. See you later."]
+    ],
+   [
+        r"add task (.*)",
+        [lambda userInput: calculateTaskTime(userInput) if studentStartTime else "Please tell me when you will start working on your tasks."]
+    ],
+    [
+        r"set start time (.*)",
+        [lambda userInput: "You will begin your tasks at " + setStudentStartTime(userInput) if not studentStartTime else "Start time is already set."] # Prompts the user to set a start time if they try to add a task before setting it
+    ],
+    [
+        r"display tasks",
+        [lambda: "Here are your tasks that you need to complete:\n" + displayTaskTable()]
+    ],
     
-name=input("Enter Your Name: ")
-print("Welcome to the Scheduler Bot Service! Let me know how can I help you?")
-def chatbot_response(request):
-  requestResponse = ('Sorry, I have not learned how to respond to your request.')
-  if re.search(r'\b(student|homework|school)\b', request, re.IGNORECASE):
-    requestResponse = 1
-  elif re.search(r'\b(work|office|meeting|meetings)\b', request, re.IGNORECASE):
-    requestResponse = 2
-  return requestResponse
+]
+         
+taskDuration= {
+  'English essay': 120,
+  'coding assignment': 60,
+  'class notes': 45,
+  'study for test': 60,
+  'watch Heimler videos': 30
+}
 
-def request_school_response():
-  print( 'Ok. I understand that you need help with organization related to school.' )
-  schoolEnd = input("When does your school end? ")
+def setStudentStartTime(userInput):
+  global studentStartTime
+  studentStartTime = datetime.datetime.strptime(x,"%Y-%m-%d %H:%M:%S")
+  return studentStartTime.strftime("%Y-%m-%d %H:%M:%S")
 
-def request_work_response():
-    print( 'Ok. I understand that you need help with organization related to work.' )
+def calculateTaskTime(task):
+  global studentStartTime
+  global currentTask
+  if task in taskDuration:
+    taskCompletionTime = studentStartTime + datetime.timedelta(minutes=taskDuration[task])
+    studentStartTime = taskCompletionTime
+    currentTask = task
+    tasks.append((task, taskCompletionTime.strftime("%Y-%m-%d %H:%M:%S")))
+    return "The task has been added. Task completion time: " + taskCompletionTime.strftime("%Y-%m-%d %H:%M:%S")
+  else:
+    return "The task you entered cannot be found. Supported tasks are:\n\n" + '\n'.join(f'- {task}' for task in taskDuration.keys()) 
 
+ 
 
-while True:
-  request=input(name+': ')
-  if request=='Bye' or request =='bye' or request == 'BYE':
-    print('Bot: Bye')
-    break
-  response = chatbot_response(request)
-  if response == 1:
-    request_school_response()
-  elif response == 2:
-      request_work_response()
+def displayTaskTable():
+  df = pd.DataFrame(tasks, columns=['Task', 'Completion Time'])
+  return df.to_html(index=False)
+
+chat = SchedulerChatbot(pairs, reflections)
+
+@app.route("/")
+def landing():
+    return render_template("chatbot.html")
+
+@app.route("/get")
+def get_chatbot_response():
+    displayResponse = request.args.get('msg')
+    return str(chat.respond(displayResponse))
+
+if __name__ == '__main__':
+    print("About to run...")
+    port = int(os.environ.get('VAPOR_LOCAL_PORT', 5000)) 
+    host = os.environ.get('VAPOR_LOCAL_HOST', "127.0.0.1")
+    print("About to run with port ", port, " and host ", host)
+    app.run(port=port, host=host)
