14395b65e2d374f9c9da96f8af884fdbdfbcafc2
unknown <r.marcellus.perez@gmail.com>
2024-02-20 22:46:03 -0600

    Edited Start Time and End Time Features *The "display tasks" will display if and only if the user has added tasks to display *The chatbot will ask the user for the start time and end time if they try to "add task" before adding the start time and end time *Added functions to remove start time and to remove end time *If te taskCompletionTime would be after the studentEndTime, the taskCompletionTime is forced back to the studentEndTime *Relates to Task #26

--- chatbot.py
+++ chatbot.py
@@ -29,16 +29,29 @@ def setStudentStartTime(userInput):
   studentStartTime = datetime.datetime.strptime(userInput,"%Y-%m-%d %H:%M:%S")
   return studentStartTime.strftime("%Y-%m-%d %H:%M:%S")
 
+def removeStudentStartTime():
+  global studentStartTime
+  studentStartTime = None
+  return "Start time removed successfully."
+
 def setStudentEndTime(userInput):
   global studentEndTime
   studentEndTime = datetime.datetime.strptime(userInput,"%Y-%m-%d %H:%M:%S")
   return studentEndTime.strftime("%Y-%m-%d %H:%M:%S")
+
+def removeStudentEndTime():
+  global studentEndTime
+  studentEndTime = None
+  return "End time removed successfully."
 
 def calculateTaskTime(task):
   global studentStartTime
   global currentTask
   if task in taskDuration:
     taskCompletionTime = studentStartTime + datetime.timedelta(minutes=taskDuration[task])
+    if taskCompletionTime > studentEndTime :
+       taskCompletionTime = studentEndTime
+       return "The task has been added. Task completion time: " + taskCompletionTime.strftime("%Y-%m-%d %H:%M:%S")
     studentStartTime = taskCompletionTime
     currentTask = task
     tasks.append((task, taskCompletionTime.strftime("%Y-%m-%d %H:%M:%S")))
@@ -67,27 +80,35 @@ class SchedulerChatbot(Chat):
 pairs = [
      [
         r"Hello|Hi|Howdy|Hey",
-        ["Hello", "How are you today?", "Nice to meet you!"]
+        ["Hello!", "Hi!", "How are you today?", "Nice to meet you!"]
     ],
        [
         r"Bye|Quit|See you tommorow|That's all the help I need",
-        ["It was my pleasure helping you today!", "Have a great day!", "Bye. See you later."]
+        ["It was my pleasure helping you today!", "Have a great day!", "Bye! See you later!"]
     ],
    [
         r"add task (.*)",
-        [lambda userInput: calculateTaskTime(userInput) if studentStartTime else "Please tell me when you will start working on your tasks."]
+        [lambda userInput: calculateTaskTime(userInput) if (studentStartTime != None and studentEndTime != None) else "Please tell me when you will be working on your tasks.\nStart Time: " + str(studentStartTime) + " | End Time: " + str(studentEndTime)]
     ],
     [
         r"set start time (.*)",
         [lambda userInput: "You will begin your tasks at " + setStudentStartTime(userInput) if not studentStartTime else "Start time is already set."] # Prompts the user to set a start time if they try to add a task before setting it
     ],
     [
-      r"set start time (.*)",
-      [lambda userInput: "You will end your tasks at " + setStudentEndTime(userInput) if not studentEndTime else "End time is already set."] # Prompts the user to set a start time if they try to add a task before setting it
+        r"remove start time",
+        [lambda: removeStudentStartTime() if studentStartTime != None else "Start time is not yet set."] # Removes the currently set start time, if existing
+    ],
+    [
+        r"set end time (.*)",
+        [lambda userInput: "You will end your tasks at " + setStudentEndTime(userInput) if not studentEndTime else "End time is already set."] # Prompts the user to set a start time if they try to add a task before setting it
+    ],
+    [
+        r"remove end time",
+        [lambda: removeStudentEndTime() if studentEndTime != None else "End time is not yet set."] # Removes the currently set end time, if existing
     ],
     [
         r"display tasks",
-        [lambda: "Here are your tasks that you need to complete:\n" + displayTaskTable()]
+        [lambda: "Here are your tasks that you need to complete:\n" + displayTaskTable() if not len(tasks) == 0 else "No tasks to display."] # Prints all tasks unless the user has not yet added tasks
     ],
     
 ]
